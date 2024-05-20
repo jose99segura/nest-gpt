f928a1f2650d9c8ca3ebcf9fdd3e06df2d34a10b
jose99segura <jose99segura@gmail.com>
2024-05-20 12:09:28 +0200

    orthogrpy-endpoint finished

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
+
+    private openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY
+    });
 
 
     // Solo va a llamar casos de uso
 
     async ortographyCheck(orthographyDto: OrthographyDto) {
-        return await orthographyCheckUseCase({
+        return await orthographyCheckUseCase( this.openai, {
             prompt: orthographyDto.prompt
         });
     }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,3 +1,4 @@
+import OpenAI from "openai";
 
 
 interface Options {
@@ -5,12 +6,44 @@ interface Options {
 }
 
 
-export const orthographyCheckUseCase = async( options: Options ) => {
+export const orthographyCheckUseCase = async( openai: OpenAI, options: Options ) => {
 
     const { prompt } = options;
 
-    return {
-        prompt: prompt
-    }
+    // Codigo de OpenAI
+    const completion = await openai.chat.completions.create({
+        messages: [
+            { 
+                role: "system", 
+                content: `
+                    Te serán proveídos textos en español con posibles errores ortográficos y gramaticales,
+                    debes responder en formato JSON,
+                    tu tarea es corregirlos y retornar información soluciones,
+                    también debes de dar un porcentaje de acierto por el usuario,
+
+                    Si no hay errores, debes de retornar un mensaje de felicitaciones.
+
+                    Ejemplo de salida:
+                    {
+                        userScore: number,
+                        errors: string[], // ['error -> solución']
+                        message: string // Usa emojis y texto para dar feedback
+                    }
+                ` 
+            },
+            { 
+                role: "user", 
+                content: prompt 
+            }
+        ],
+        model: "gpt-4o",
+        max_tokens: 150,
+        temperature: 0.3,
+        response_format: { type: "json_object" }
+    });
+    
+    const jsonResp = JSON.parse(completion.choices[0].message.content);
+
+    return jsonResp;
 
 }
