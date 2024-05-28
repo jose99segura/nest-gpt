4dbd1db274116356c7f3db5a2f02b5dd68a34a07
jose99segura <jose99segura@gmail.com>
2024-05-28 11:15:55 +0200

    starting working with assistants and threads openAI backend

---
+++ src/sam-assistant/sam-assistant.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
+import { createMessageUseCase, createThreadUseCase } from './use-cases';
+import { QuestionDto } from './dtos/question.dto';
+
+@Injectable()
+export class SamAssistantService {
+
+    private openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY
+    });
+
+
+    async createThread() {
+        return await createThreadUseCase(this.openai);
+    }
+
+    async userQuestion( questionDto: QuestionDto ) {
+        const { threadId, question } = questionDto;
+        const message = await createMessageUseCase( this.openai, { threadId, question } );
+        console.log(message);
+        
+    }
+
+
+
+}
---
+++ src/sam-assistant/use-cases/index.ts
@@ -0,0 +1,4 @@
+
+
+export * from './create-thread.use-case'
+export * from './create-message.use-case'
